07513a6922e54afbbd2968e46d6126c05c6bfd74
Cesar Lopez <cesarlerdeut@gmail.com>
2022-09-19 20:09:20 +0200

    Setup express server

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,22 @@
 import { MikroORM } from "@mikro-orm/core";
-import {Post} from "./entities/Post";
+// import {Post} from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
 import {__prod__} from "./utils/constants";
+import express from "express";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
 
-  const em = orm.em.fork();
+  // const em = orm.em.fork();
 
-  // const post = em.create(Post, {
-  //   title: 'My first post',
-  //   createdAt: new Date(),
-  //   updatedAt: new Date()
-  // });
-  // await em.persistAndFlush(post);
-
-  const posts = await em.find(Post, {});
-  console.log(posts);
-
+  const app = express();
+  app.get('/', (_, res) => {
+    res.send("Hello World!");
+  });
+  app.listen(4000, () => {
+    console.log('server started on localhost:4000');
+  });
 };
 
 main().catch((err) => {
