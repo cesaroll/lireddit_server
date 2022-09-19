f54e0a08c6ba2b8065e674e0f1cfb2b27961f7d4
Cesar Lopez <cesarlerdeut@gmail.com>
2022-09-19 21:20:49 +0200

    Setup Graphql and create query

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
-// import {Post} from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
 import {__prod__} from "./utils/constants";
 import express from "express";
+import {ApolloServer} from "apollo-server-express";
+import {buildSchema} from "type-graphql";
+import {HelloResolver} from "./resolvers/hello";
+import {PostResolver} from "./resolvers/post";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
@@ -11,6 +15,18 @@ const main = async () => {
   // const em = orm.em.fork();
 
   const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false
+    }),
+    context: () => ({em: orm.em.fork()})
+  });
+
+  await apolloServer.start();
+  apolloServer.applyMiddleware({app});
+
   app.get('/', (_, res) => {
     res.send("Hello World!");
   });
---
+++ src/resolvers/post.ts
@@ -0,0 +1,14 @@
+import {Ctx, Query, Resolver} from "type-graphql";
+import { Post } from '../entities/Post';
+import { MyContext } from '../types';
+
+@Resolver()
+export class PostResolver {
+  @Query(() => [Post])
+  posts(
+    @Ctx()
+    ctx: MyContext
+  ): Promise<Post[]> {
+    return ctx.em.find(Post, {});
+  }
+}
